2f9bb0cf66d09c1b10e7a3d9982c44387e804dc9
EshwarMailaram <eshwar.mailaram@gmail.com>
2025-05-26 07:56:24 +0000

    variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,13 @@
+const accountId=144553
+let accountEmail="hitesh@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+
+//accountId=2 //not allowed
+
+accountEmail="hch@c.com"
+accountPassword="6789"
+accountCity="Bengaluru"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity]);
